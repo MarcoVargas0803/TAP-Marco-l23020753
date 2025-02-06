d9828f038610b788d8bfd2eb29de0ab7c1c4b75c
marco vargas <l23020753veracruz.tecnm.mx>
2025-02-05 22:19:23 -0600

    Redirreccionando los archivos de los Repasos de POO

---
+++ RepasoPOO/Perro.py
@@ -0,0 +1,15 @@
+class Perro:
+    def ladrar(self):
+        print("estoy ladrando...")
+        return
+    def dormir(self):
+        print("estoy durmiendo...")
+        return
+
+perro1 = Perro();
+
+perro2 = Perro();
+
+print(perro1.ladrar())
+
+print(perro2.dormir())
