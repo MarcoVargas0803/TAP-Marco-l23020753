67d6e723f6802b10eda30ce69ca5ce661dd81daf
Marco A. Vargas <l23020753@veracruz.tecnm.mx>
2025-02-27 22:16:12 -0600

    Login y Registro V2

---
+++ TAP_2025/Practice_ct/Exercise_250225_verification.py
@@ -0,0 +1,8 @@
+from Excercise_250225_register import Registro
+
+class Verification:
+        def __init__(self,Login,Registro):
+            self.Login = Login
+            self.Registro = Registro
+
+        def button_login_verification(self):
