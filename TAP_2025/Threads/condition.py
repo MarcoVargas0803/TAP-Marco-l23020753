32aaa602bf43c8aaab7a7c07d15efd7683638737
Marco A. Vargas <l23020753@veracruz.tecnm.mx>
2025-04-02 09:31:28 -0600

    Condition , ejemplo notify_all.

--- TAP_2025/Threads/condition.py
+++ TAP_2025/Threads/condition.py
@@ -1,38 +1,24 @@
 #superfastpython.com
 #Example of wait/notify with a condition
 from time import sleep
+from random import random
 from threading import Thread
 from threading import Condition
 
-from TAP_2025.Threads.semaphore import worker
-
-
-def task(condition, work_list):
-    sleep(1)
-    #Add data to the work list
-    work_list.append(33)
-    #Notify a waiting thread that the work is done
-    print("Thread sending notification...")
+def task(condition, numero):
+    print(f"Thread {numero} waiting ... ")
     with condition:
-        condition.notify()
+        condition.wait()
+    value = random()
+    sleep(value)
+    print(f"Thread {numero} got value")
 
-def task2(work_list):
-    sleep(1)
-    #Add data to the work list
-    work_list.append(33)
-    #Notify a waiting thread that the work is done
-    print("Thread sending notification...")
-
-#Created shared semaphore
-condition = Condition(1)
-#Prepare the work list
-work_list = list()
-#Wait to be notified that the data is ready
-print("Main thread waiting for data")
-with condition:
-    #start a new thread to perform some work
-    worker = Thread(target=task, args=(condition, work_list))
+#Created shared condition
+condition = Condition()
+for i in range(20):
+    worker = Thread(target=task,args=(condition,i))
     worker.start()
 
-    condition.wait()
-print(f"Got data: {work_list}")
+sleep(3)
+with condition:
+    condition.notify_all()
